1f9264cbb441e5848bf2ba318f7eb6516be938c9
Md. Mehedi Hasan Khan <mehedikhan3281@gmail.com>
2024-10-08 23:44:15 +0600

    Footer Adjust

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@
 
 export default function Footer() {
     return (
-      <footer className="flex items-center justify-between text-black p-4 mt-6 text-center border-2 border-solid
-       bg-white mx-2 border-gray-300 w-full">
+      <footer className="flex flex-col md:flex-row md:items-center md:justify-between gap-4 text-black p-4 mt-6 text-center border-2 border-solid
+       bg-white mx-2 border-gray-300">
         <p>© 2024 All rights reserved by Greater Chattogram Association. | Design & Developed by Dhrubo Joyti Das</p>
        <p>Sitemap | Privacy Policy</p>
       </footer>
